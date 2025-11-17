8f5e9738d61dfa2e259362733f4e9e39debeb814
Jonathan <JohnAnBer@gmail.com>
2025-11-17 09:59:52 -0500

    wow I suck

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 /**
  * Task Class
- * 
+ *
  * A class to manage tasks with title, priority, completion status, and time tracking.
  */
 
 class Task {
- //write your code here
- 
+
+    #completed = false;
+    #minutesSpent = 0;
+    static allTasks = []
+
+
+    constructor(title, priority) {
+        this.title = title
+        this.priority = priority
+        Task.allTasks.push(this)
+    }
+
+    workOn(minutes){
+        this.#minutesSpent += minutes
+        console.log(`Worked on ${this.title} for ${minutes} minutes. Total time: ${this.#minutesSpent} minutes`)
+    }
+
+    complete() {
+        this.#completed = true
+        console.log(`${this.title} has been completed!`)
+    }
+
+    completedStatus() {
+        return this.#completed
+    }
+
+    isComplete() {
+       return this.#completed
+    }
 }
 
 //Test class with examples below:
