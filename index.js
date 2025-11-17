266823818d00a5e9b177b7b304f3c093902fbf26
Jonathan <JohnAnBer@gmail.com>
2025-11-17 10:23:43 -0500

    hi

--- index.js
+++ index.js
@@ -27,17 +27,45 @@ class Task {
         console.log(`${this.title} has been completed!`)
     }
 
-    completedStatus() {
+    get completedStatus() {
         return this.#completed
     }
 
     isComplete() {
        return this.#completed
     }
+
+    get timeSpent() {
+        return this.#minutesSpent
+    }
+    static getTotalTasks() {
+        return Task.allTasks.length
+    }
+
+    static findByTitle(title){
+        if (Task.allTasks.includes(title)){
+            Task.allTasks.find((el) => el === title)
+        } else {
+            return undefined
+        }
+    }
 }
 
 //Test class with examples below:
 
 
+
+// Static property examples
+const t1 = new Task("Laundry", "medium");
+const t2 = new Task("Pay bills", "high");
+
+console.log(Task.allTasks);
+// [ Task {...}, Task {...}, Task {...} ]
+
+// Static method examples
+console.log(Task.getTotalTasks()); // 3
+
+console.log(Task.findByTitle("Laundry"));
+// Returns the "Laundry" task instance
 // Export the Task class for testing
 module.exports = { Task };
